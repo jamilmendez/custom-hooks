0cd9f6af3cb10611259bf1065ba93eacc2e7ddd8
jamilmendez <mdztnr99@gmail.com>
2022-10-07 00:03:36 -0500

    todoReducer added

---
+++ useTodos/todoReducer.ts
@@ -0,0 +1,35 @@
+interface ActionType {
+  type: string;
+  payload?: string;
+}
+
+interface TodoProps {
+  id: number | string;
+  description: string;
+  done: boolean;
+}
+
+export const todoReducer = (initialState: TodoProps[] = [], action: ActionType) => {
+  switch (action.type) {
+    case "[TODO] Add Todo":
+      return [...initialState, action.payload];
+
+    case "[TODO] Remove Todo":
+      return initialState.filter(todo => todo.id !== action.payload);
+
+    case "[TODO] Toggle Todo":
+      return initialState.map((todo) => {
+        if (todo.id === action.payload) {
+          return {
+            ...todo,
+            done: !todo.done,
+          };
+        }
+
+        return todo;
+      });
+
+    default:
+      return initialState;
+  }
+};
