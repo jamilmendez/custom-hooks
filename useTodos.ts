6abe1a66cc79483e0467d399ad17b5240d190feb
jamilmendez <mdztnr99@gmail.com>
2022-10-06 23:52:38 -0500

    first commit hooks

---
+++ useTodos.ts
@@ -0,0 +1,51 @@
+import { Reducer, useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const init = () => {
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
+
+interface TodoProps {
+  id: number;
+  description: string;
+  done: boolean;
+}
+
+export const useTodos = () => {
+  const [todos, dispatch] = useReducer(todoReducer, [], init);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleNewTodo = (todo: TodoProps) => {
+    const action = {
+      type: "[TODO] Add Todo",
+      payload: todo,
+    };
+    dispatch(action);
+  };
+
+  const handleDeleteTodo = (id: string) => {
+    dispatch({
+      type: "[TODO] Remove Todo",
+      payload: id,
+    });
+  };
+
+  const handleToggleTodo = (id: string) => {
+    dispatch({
+      type: "[TODO] Toggle Todo",
+      payload: id,
+    });
+  };
+
+  return {
+    todos,
+    todosCount: todos.length,
+    pendingTodosCount: todos.filter((todo: { done: boolean; }) => !todo.done).length,
+    handleDeleteTodo,
+    handleToggleTodo,
+    handleNewTodo,
+  };
+};
